3753b46901fffb45d42090cc4574f863caf3b957
mblogger <mblogger@users.noreply.github.com>
2019-08-08 16:44:34 +0530

    Initial commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,26 @@
+import { PostService } from './../post.service';
+import { Post } from './../post.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html'
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(public postService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostUpdateListener().subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
